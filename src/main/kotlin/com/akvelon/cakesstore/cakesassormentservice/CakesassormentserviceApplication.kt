432594965f13a893767807fe02180ba3426f397f
Andrey Martynov <andrey.martynov@akvelon.com>
2019-11-29 16:26:18 +0300

    Initial commit

---
+++ src/main/kotlin/com/akvelon/cakesstore/cakesassormentservice/CakesassormentserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.akvelon.cakesstore.cakesassormentservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CakesassormentserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CakesassormentserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/akvelon/cakesstore/cakesassormentservice/CakesassormentserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.akvelon.cakesstore.cakesassormentservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CakesassormentserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
