d97670f97011856db961545557286d5d6d63c863
Andrey Martynov <andrey.martynov@akvelon.com>
2019-12-02 13:54:17 +0300

    Added eureka server

---
+++ eureka-server/src/main/kotlin/com/akvelon/cakestore/eureka/EurekaServer.kt
@@ -0,0 +1,12 @@
+package com.akvelon.cakestore.eureka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class EurekaServer
+fun main(args: Array<String>) {
+    runApplication<EurekaServer>(*args)
+}
